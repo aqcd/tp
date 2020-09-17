b99a7a095d61216b4f1a24bd29429da29b5a20ce
aqcd <aquajet173@gmail.com>
2020-09-17 19:38:06 +0800

    add remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,41 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+// @@author aqcd-reused
+// Reused from https://github.com/se-edu/addressbook-level3/compare/7b674c7..fac8f3f as part of tutorial.
+
+/**
+ * Represents a Person's remark in the address book.
+ * Guarantees: immutable; is always valid
+ */
+public class Remark {
+    public final String value;
+
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Remark // instanceof handles nulls
+                && value.equals(((Remark) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
